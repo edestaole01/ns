a92ff59d7d2123212daf2b4b87f87775394aa94b
eduardo@superclient.com.br <eduardo@superclient.com.br>
2025-10-08 18:12:20 -0300

    Update version.js

--- version.js
+++ version.js
@@ -1,4 +1,7 @@
-/ --- ARQUIVO: version.js ---
+// --- START OF FILE version.js ---
+
 // Ponto central para a versão do aplicativo.
 // Altere este número sempre que fizer uma nova atualização.
-const APP_VERSION = '1.0.1'; // Exemplo de versão (Major.Minor.Patch)
+const APP_VERSION = '1.0.2'; // Mudei para 1.0.2 para garantir que a atualização seja detectada
+
+// --- END OF FILE version.js ---
